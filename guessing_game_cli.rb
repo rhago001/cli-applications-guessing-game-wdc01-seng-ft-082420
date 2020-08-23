45ff00f8c07aed06787b1ee91176bfd69cda9343
Rahel Hagos <hagos.rahelh@gmail.com>
2020-08-23 19:03:51 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -16,6 +16,17 @@ input_num=gets.chop
   
   end
 end
-
+def run_guessing_game
+  rando = rand(6)+1
+  puts "Guess a number between 1 and 6:"
+  input = gets.chomp
+  if input == rando.to_s
+    puts "You guessed the correct number!"
+  elsif input.downcase == "exit"
+    puts "Goodbye!"
+  else
+    puts "Sorry! The computer guessed #{rando}."
+  end
+end
 
 
